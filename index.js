9dd1cb011c8764e2ea8ec651e36c1f14e71c1ceb
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-04-14 19:02:56 +0300

    add correct port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 const db = require('./context')();
 const server = require('./server')(db);
-
+const PORT = process.env.PORT || 3030;
 (async function () {
     await db.sequelize.sync({
         force: true
     });
-    const port = 3030;
-    server.listen(port, () => console.log(`Server is running on port ${port}`));
+    //const port = 3030;
+    server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 })();
