760ac0e978a9cce141ff946b7aab171df5914a66
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-04-14 12:36:22 +0300

    format file

--- models/users.js
+++ models/users.js
@@ -1,8 +1,15 @@
-module.exports = (Sequelize, sequelize) =>
-{
+module.exports = (Sequelize, sequelize) => {
     return sequelize.define('users', {
-        ID : {type: Sequelize.INTEGER, primaryKey : true, autoIncrement : true},
-        Login : {type: Sequelize.STRING},
-        Password : {type: Sequelize. STRING}
+        ID: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Login: {
+            type: Sequelize.STRING
+        },
+        Password: {
+            type: Sequelize.STRING
+        }
     })
 }
