dd92739c070860c21dd1d8e4cd456dd17b92b5e7
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-04-14 23:03:03 +0300

    add possibility for correct reading statistics of 1 user

--- services/statistics.js
+++ services/statistics.js
@@ -5,10 +5,15 @@ class StatService extends CrudService {
     constructor(rep, errors) {
         super(rep, errors);
     }
+    async readChunk(opts){
+        let stats = await super.readChunk(opts);
+        return stats.filter( elem => elem.userId == opts.userId)[0];   
+    }
     async create(data) {
         const error = validator.check('stat', data);
         if (error.error) throw this.errors.invalidData;
-        
+        const stat = await this.read(data.userId);
+        if (stat) throw this.errors.alreadyExists;
         return super.create(data);
     }
     async update(id, data) {
