49cd32de5cbad55f03a2493beab784594eb7bf85
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-04-14 17:45:54 +0300

    add error-handling

---
+++ global-controllers/errors.js
@@ -0,0 +1,12 @@
+module.exports = (error, req, res, next) => {;
+    if (!error.code) {
+        error = {
+            message: error.toString(),
+            code: 'server_error',
+            status: 500
+        };
+    }
+    res.status(error.status).json({
+        message: error.message
+    });
+}
