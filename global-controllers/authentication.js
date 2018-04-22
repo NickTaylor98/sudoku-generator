249845f398c55514ca76118a29faf278db427af5
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-04-23 00:08:01 +0300

    add function for pushing token to cookie

--- global-controllers/authentication.js
+++ global-controllers/authentication.js
@@ -1,14 +1,39 @@
 'use strict';
 
-const verify = require('../helpers/authentication');
+const jwt = require('../helpers/authentication');
+const bcrypt = require('bcryptjs');
 const TOKEN_COOKIE = '__service_token';
 
-module.exports = async (req, res, next) => {
+let service;
+
+async function verify(req, res, next) {
     const token = req.cookies[TOKEN_COOKIE];
-    const verificationValue = await verify(token);
+    const verificationValue = await jwt.verify(token);
     if (!verificationValue) {
-        res.redirect('/login.html');
+        res.cookie(`callback`, req.baseUrl);
+        res.redirect(`/login.html`);
     } else {
         next();
     }
 }
+async function sign(req, res, next){
+    const user = await service.readByLogin(req.body.login);
+    const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!isValidPassword)
+    {
+        res.status(403).json("Unauthorized");
+    }
+    else 
+    {
+        const token = await jwt.sign(user.id);
+        res.cookie(TOKEN_COOKIE, token);
+        res.redirect(req.cookies[`callback`]);
+        //next();
+    }
+}
+
+module.exports = (userService) =>
+{
+    service = userService;
+    return {verify, sign};   
+} 
