8e9d76e23aa6e2fb47e1518efb89b8849c2adaec
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-04-29 00:17:37 +0300

    add checking rights to doing operations (authorization)

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -1,5 +1,8 @@
 'use strict';
 const CrudController = require('./crud');
+const {
+    checkAuth
+} = require('../helpers/authorization');
 class StatsController extends CrudController {
     constructor(service) {
         super(service);
@@ -7,14 +10,18 @@ class StatsController extends CrudController {
     }
     async readAll(req, res) {
         req.query.userId = req.params.userId;
-        return super.readAll(req, res);
+        const checkValue = await checkAuth(req.ability, 'read', 'stats');
+        return checkValue.access ? super.readAll(req, res) : checkValue.error.message;
     }
     async create(req, res) {
         req.body.userId = req.params.userId;
+        const checkValue = await checkAuth(req.ability, 'create', 'stats');
         return super.create(req, res);
     }
     async update(req, res) {
         req.body.userId = req.params.userId;
+        const stat = this.service.read(req.params.id);
+        const checkValue = await checkAuth(req.ability, 'update', stat);
         return super.update(req, res);
     }
 }
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,60 @@
 'use strict';
 const CrudController = require('./crud');
+const axios = require('axios');
+const {
+    checkAuth
+} = require('../helpers/authorization');
+const defaultStats = {
+    hardWins: 0,
+    mediumWins: 0,
+    easyWins: 0,
+    hardLoses: 0,
+    mediumLoses: 0,
+    easyLoses: 0
+};
 class UsersController extends CrudController {
     constructor(service) {
         super(service);
         this.registerRoutes();
+    }
+    async readAll(req, res) {
+        const checkValue = await checkAuth(req.ability, 'read', 'users');
+        if (checkValue.access) {
+            super.readAll(req, res);
+        } else {
+            res.status(checkValue.error.status).json(checkValue.error.message);
+        }
+    }
+    async create(req, res) {
+        const checkValue = await checkAuth(req.ability, 'create', 'users');
+        let [message, status] = ["", 200];
+        if (checkValue.access) {
+            let data = await this.service.create(req.body);
+            let resp = await axios.post(`http://localhost:3030/api/users/${data.id}/stats`, defaultStats);
+            message = data;
+        } else {
+            message = checkValue.error.message;
+            status = checkValue.error.status;
+        }
+        res.status(status).json(message);
+    }
+    async update(req, res) {
+        const user = await this.service.read(req.params.id);
+        const checkValue = await checkAuth(req.ability, 'update', user);
+        if (checkValue.access) {
+            return super.update(req, res);
+        } else {
+            res.status(checkValue.error.status).json(checkValue.error.message);
+        }
+    }
+    async delete(req, res) {
+        const user = await this.service.read(req.params.id);
+        const checkValue = await checkAuth(req.ability, 'delete', user);
+        if (checkValue.access) {
+            return super.delete(req, res);
+        } else {
+            res.status(checkValue.error.status).json(checkValue.error.message);
+        }
     }
 }
 module.exports = (usersService) => {
