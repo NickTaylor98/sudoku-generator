ac7c21f0e9de3d50eee8b89f5469a1265a7e39d2
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-05-12 00:00:47 +0300

    change verification (from access_token to refresh_token)

--- global-controllers/pages.js
+++ global-controllers/pages.js
@@ -5,21 +5,18 @@ const {
 } = require('../helpers/authentication');
 const path = require('path');
 const ACCESS_TOKEN = '__service_token';
-
+const REFRESH_TOKEN = '__refresh_token';
 const BASIC_PATH = path.resolve(__dirname, `../public/html/`);
 
 async function redirect(req, res, next) {
-    const accessToken = req.cookies[ACCESS_TOKEN];
+    const accessToken = req.cookies[REFRESH_TOKEN];
     let verificationValue = await verifyToken(accessToken);
     if (req.path === '/index.html' || req.path == '/') {
         if (verificationValue) res.sendFile(`${BASIC_PATH}/index.html`);
         else res.redirect(`/login.html`);
-    } else if (req.path === '/login.html') {
+    } else if (req.path === '/login.html' || req.path === '/signup.html') {
         if (verificationValue) res.redirect(`/`);
-        else res.sendFile(`${BASIC_PATH}/login.html`);
-    } else if (req.path === '/signup.html') {
-        if (verificationValue) res.redirect(`/`);
-        else res.sendFile(`${BASIC_PATH}/signup.html`);
+        else res.sendFile(`${BASIC_PATH}${req.path}`);
     }
 }
 
