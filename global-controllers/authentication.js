cdf1b94c748e75ac3df960f9cf4c28a7137566ec
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-05-11 23:59:06 +0300

    add default redirect url

--- global-controllers/authentication.js
+++ global-controllers/authentication.js
@@ -38,7 +38,7 @@ async function sign(req, res, next) {
             const refreshToken = await signToken(user.id, 60 * 24 * 10); //10 days
             res.cookie(ACCESS_TOKEN, accessToken);
             res.cookie(REFRESH_TOKEN, refreshToken);
-            res.redirect(req.body.callback);
+            res.redirect(req.body.callback || '/');
         }
     } catch (e) {
         return next(e);
