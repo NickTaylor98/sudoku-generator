888508b42b5a9eec777580d19f8e0d02bb396f59
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-05-12 21:09:42 +0300

    delete all data from database

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('./server')(db);
 const PORT = process.env.PORT || 3030;
 (async function () {
     await db.sequelize.sync({
-        force: false
+        force: true
     });
     server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 })();
