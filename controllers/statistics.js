66643f68212ba364e01cbfbc5c85c0032afb1b24
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-05-15 19:16:53 +0300

    add apidoc comments

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -27,16 +27,104 @@ class StatsController extends CrudController {
         };
         this.registerRoutes();
     }
+
+    /**
+     * @api {get} /users/:userID/stats List user statistic
+     * @apiGroup Stats
+     * @apiParam {userID} id User identifier
+     * @apiSuccess {Number} stats.id Stats id
+     * @apiSuccess {Number} stats.hardWins Quantity of wins hard level sudoku
+     * @apiSuccess {Number} stats.mediumWins Quantity of wins medium level sudoku
+     * @apiSuccess {Number} stats.easyWins Quantity of wins easy level sudoku
+     * @apiSuccess {Number} stats.hardLoses Quantity of loses hard level sudoku
+     * @apiSuccess {Number} stats.mediumLoses Quantity of loses medium level sudoku
+     * @apiSuccess {Number} stats.easyLoses Quantity of loses easy level sudoku
+     * @apiSuccess {Number} stats.userId User Id
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id":1,
+     *      "hardWins":0,
+     *      "hardLoses":0,
+     *      "mediumWins":0,
+     *      "mediumLoses":0,
+     *      "easyWins":1,
+     *      "easyLoses":2,
+     *      "userId":1
+     *    }  
+     */
     async read(req, res) {
         req.params.id = req.params.userId;
         const checkValue = await checkAuth(req.ability, 'read', 'statistics');
         return checkValue.access ? super.read(req, res) : checkValue.error.message;
     }
+    /**
+     * @api {post} /users/:userID/stats Create user statistic
+     * @apiGroup Stats
+     * @apiParam {Number} userID id User identifier
+     * @apiParam {Number} hardWins Quantity of wins hard level sudoku
+     * @apiParam {Number} mediumWins Quantity of wins medium level sudoku
+     * @apiParam {Number} easyWins Quantity of wins easy level sudoku
+     * @apiParam {Number} hardLoses Quantity of loses hard level sudoku
+     * @apiParam {Number} mediumLoses Quantity of loses medium level sudoku
+     * @apiParam {Number} easyLoses Quantity of loses easy level sudoku
+     * @apiSuccess {Number} stats.id Stats id
+     * @apiSuccess {Number} stats.hardWins Quantity of wins hard level sudoku
+     * @apiSuccess {Number} stats.mediumWins Quantity of wins medium level sudoku
+     * @apiSuccess {Number} stats.easyWins Quantity of wins easy level sudoku
+     * @apiSuccess {Number} stats.hardLoses Quantity of loses hard level sudoku
+     * @apiSuccess {Number} stats.mediumLoses Quantity of loses medium level sudoku
+     * @apiSuccess {Number} stats.easyLoses Quantity of loses easy level sudoku
+     * @apiSuccess {Number} stats.userId User Id
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id":1,
+     *      "hardWins":0,
+     *      "hardLoses":0,
+     *      "mediumWins":0,
+     *      "mediumLoses":0,
+     *      "easyWins":1,
+     *      "easyLoses":2,
+     *      "userId":1
+     *    }  
+     */
     async create(req, res) {
         req.body.userId = req.params.userId;
         const checkValue = await checkAuth(req.ability, 'create', 'statistics');
         return super.create(req, res);
     }
+    /**
+     * @api {put} /users/:userID/stats Update user statistic
+     * @apiGroup Stats
+     * @apiParam {Number} userID id User identifier
+     * @apiParam {Number} hardWins Quantity of wins hard level sudoku
+     * @apiParam {Number} mediumWins Quantity of wins medium level sudoku
+     * @apiParam {Number} easyWins Quantity of wins easy level sudoku
+     * @apiParam {Number} hardLoses Quantity of loses hard level sudoku
+     * @apiParam {Number} mediumLoses Quantity of loses medium level sudoku
+     * @apiParam {Number} easyLoses Quantity of loses easy level sudoku
+     * @apiSuccess {Number} stats.id Stats id
+     * @apiSuccess {Number} stats.hardWins Quantity of wins hard level sudoku
+     * @apiSuccess {Number} stats.mediumWins Quantity of wins medium level sudoku
+     * @apiSuccess {Number} stats.easyWins Quantity of wins easy level sudoku
+     * @apiSuccess {Number} stats.hardLoses Quantity of loses hard level sudoku
+     * @apiSuccess {Number} stats.mediumLoses Quantity of loses medium level sudoku
+     * @apiSuccess {Number} stats.easyLoses Quantity of loses easy level sudoku
+     * @apiSuccess {Number} stats.userId User Id
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id":1,
+     *      "hardWins":0,
+     *      "hardLoses":0,
+     *      "mediumWins":0,
+     *      "mediumLoses":0,
+     *      "easyWins":1,
+     *      "easyLoses":2,
+     *      "userId":1
+     *    }  
+     */
     async update(req, res) {
         req.body.userId = req.params.id = req.params.userId;
         const stat = await this.service.read(req.params.userId);
--- controllers/users.js
+++ controllers/users.js
@@ -17,14 +17,67 @@ class UsersController extends CrudController {
         super(service);
         this.registerRoutes();
     }
+
+    /**
+     * @api {get} /users List all users
+     * @apiGroup Users
+     * @apiSuccess {Object[]} users User's list
+     * @apiSuccess {Number} users.id User id
+     * @apiSuccess {String} users.login User login
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    [{
+     *      "id": 1,
+     *      "login": "user1"
+     *    },
+     *    {
+     *      "id": 2,
+     *      "login": "user2"
+     *    }]
+     *      
+     */
     async readAll(req, res) {
         const checkValue = await checkAuth(req.ability, 'read', 'users');
         if (checkValue.access) {
-            super.readAll(req, res);
+            return super.readAll(req, res);
         } else {
             res.status(checkValue.error.status).json(checkValue.error.message);
         }
     }
+    /**
+     * @api {get} /users/:userID List one user
+     * @apiGroup Users
+     * @apiParam {userID} id User identifier
+     * @apiSuccess {Number} users.id User id
+     * @apiSuccess {String} users.login User login
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id": 1,
+     *      "login": "user1"
+     *    }
+     *      
+     */
+    async read(req, res) {
+        return super.read(req, res);
+    }
+    /**
+     * @api {post} /users Create new user
+     * @apiGroup Users
+     * @apiParam {login} login User login
+     * @apiParam {password} password User password
+     * @apiSuccess {Number} id User id
+     * @apiSuccess {String} login User login
+     * @apiSuccess {String} password User password
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id": 1,
+     *      "login": "user1",
+     *      "password": "2%2svjkfwhvkwjvbjwhsvhjsdfgvkjshdg"
+     *    }
+     *      
+     */
     async create(req, res) {
         const checkValue = await checkAuth(req.ability, 'create', 'users');
         let [message, status] = ["", 200];
@@ -38,6 +91,25 @@ class UsersController extends CrudController {
         }
         res.status(status).json(message);
     }
+
+    /**
+     * @api {put} /users/:userID Update user
+     * @apiGroup Users
+     * @apiParam {userID} id User identifier
+     * @apiParam {login} login User login
+     * @apiParam {password} password User password
+     * @apiSuccess {Number} id User id
+     * @apiSuccess {String} login User login
+     * @apiSuccess {String} password User password
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "id": 1,
+     *      "login": "user2",
+     *      "password": "2%2svjkfwhvkwjvbjwhsvhjsdfgvkjshdg"
+     *    }
+     *      
+     */
     async update(req, res) {
         const user = await this.service.read(req.params.id);
         const checkValue = await checkAuth(req.ability, 'update', user);
@@ -47,6 +119,15 @@ class UsersController extends CrudController {
             res.status(checkValue.error.status).json(checkValue.error.message);
         }
     }
+    /**
+     * @api {delete} /users/:userID Delete user
+     * @apiGroup Users
+     * @apiParam {userID} id User identifier
+     * @apiSuccess {Number} success Success value
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    1 
+     */
     async delete(req, res) {
         const user = await this.service.read(req.params.id);
         const checkValue = await checkAuth(req.ability, 'delete', user);
